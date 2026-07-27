45f030696bf6c791382f8c4c9321697d01c3a3bc
ASmolenkov <a.smolenkov@list.ru>
2026-07-27 19:07:54 +0300

    refactor (FindMatchException) Исключение принимает в себя базовое исключение

--- src/main/java/io/github/asmolenkov/tennismatchscoreboard/exception/FindMatchException.java
+++ src/main/java/io/github/asmolenkov/tennismatchscoreboard/exception/FindMatchException.java
@@ -2,7 +2,7 @@ package io.github.asmolenkov.tennismatchscoreboard.exception;
 
 public class FindMatchException extends RuntimeException {
     public FindMatchException(String message, Exception e){
-        super(message);}
+        super(message, e);}
 
     public FindMatchException(String message){
         super(message);}
